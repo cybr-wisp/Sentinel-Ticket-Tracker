b191250da9b40adc883addd29eea0e0a82325de2
cybr-wisp <marierubybeaufort@gmail.com>
2026-06-26 14:14:24 -0400

    add serializers for Project, Ticket, Comment

--- tickets/serializers.py
+++ tickets/serializers.py
@@ -1,4 +1,28 @@
 
 
+# ☆ Purpose: converts your Django model objects ↔ JSON. When the API receives a POST request with JSON, 
+# the serializer validates it and turns it into a Ticket object. When you fetch a Ticket to send back 
+# as a response, the serializer turns it into JSON.
+
+# Imports 
+from .models import Ticket, Project, Comment
 from rest_framework import serializers
-from .models import Project, Ticket, Comment
+
+
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Project
+        fields = "__all__"
+
+class TicketSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ticket
+        fields = "__all__"
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = '__all__'
+
+
+
