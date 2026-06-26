c9c034f3da3842c66b146946dbd5f2702772c985
cybr-wisp <marierubybeaufort@gmail.com>
2026-06-26 13:53:54 -0400

    register models in admin

--- tickets/admin.py
+++ tickets/admin.py
@@ -1,3 +1,10 @@
-from django.contrib import admin
 
-# Register your models here.
+
+# Imports:
+from django.contrib import admin
+from .models import Project, Ticket, Comment
+
+
+admin.site.register(Ticket)
+admin.site.register(Project)
+admin.site.register(Comment)
